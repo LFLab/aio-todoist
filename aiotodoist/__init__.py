3eff1927053445ea72aae04f1bf0d851e5f1f8d6
Lanfon <lanfon72@gmail.com>
2021-03-12 03:02:41 +0800

    * bump version

--- aiotodoist/__init__.py
+++ aiotodoist/__init__.py
@@ -4,4 +4,7 @@ from .subscribe import Handler, json_default, subscribe
 __all__ = ("TodoistAPI", "AsyncTodoistAPI",
            "subscribe", "Handler", "json_default")
 
-__version__ = '8.0.0'  # follow TodoistAPI version.
+__version__ = '8.1.0.2'
+# Versioning uses: major.minorA . majorB.minorB
+# where `A` versions following origin TodoistAPI,
+# and `B` versions is the AsyncTodoistAPI.
