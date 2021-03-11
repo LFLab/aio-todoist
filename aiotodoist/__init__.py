95907577d000595213b9373890bf7299bb150729
Lanfon <lanfon72@gmail.com>
2021-03-12 03:01:59 +0800

    + expose more implements

--- aiotodoist/__init__.py
+++ aiotodoist/__init__.py
@@ -1,4 +1,7 @@
 from .api import TodoistAPI, AsyncTodoistAPI
+from .subscribe import Handler, json_default, subscribe
 
-__all__ = ("TodoistAPI", "AsyncTodoistAPI")
+__all__ = ("TodoistAPI", "AsyncTodoistAPI",
+           "subscribe", "Handler", "json_default")
+
 __version__ = '8.0.0'  # follow TodoistAPI version.
